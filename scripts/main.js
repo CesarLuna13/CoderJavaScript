427e5c4bd2f57f87bf967436770157e41858682e
Ce Luna <cesar_luga@hotmail.com>
2022-10-03 21:57:33 -0500

    Arrays

--- scripts/main.js
+++ scripts/main.js
@@ -33,3 +33,21 @@ function registroUsuario(){
 
     return nombre + " " + apellido +  " " + username
 }
+
+
+
+// Arrays
+function arrays(){
+    const arrayA = []; //array vacio, priorizar creearlos con const, por seguridad
+    let arrayB = [1, 2];
+    const arrayC = ["A", "B", "C"];
+    const arrayD = [true, false, true];
+    const arrayE = [1, false, "C"]; // No muy común
+    console.table(arrayC); //Es mejor cuando se quieren ver los elementos de un array
+
+    //Métodos
+    arrayC.push("D"); //Agrega elemento en la última posición del array
+    arrayC.unshift("a"); //Agrega elemento en la primera posición
+    
+
+}
